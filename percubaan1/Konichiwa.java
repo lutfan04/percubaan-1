26c8fc532b959d7149d7113e45b8a2ff079a992c
lutfan04 <lutfan04@gmail.com>
2021-10-03 09:35:24 +0700

    sayonara

---
+++ percubaan1/Konichiwa.java
@@ -0,0 +1,11 @@
+	public class Konichiwa {
+		public static void main(String[] args) {
+			System.out.println("daijobu desho");
+			System.out.println("sumimasen");
+			System.out.println("amaterasu");
+			System.out.println("konichiwa");
+			}
+	
+	
+	
+}
